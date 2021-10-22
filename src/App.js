5c0099ef9c5ff1bbf9295467283f7572d29368ee
Hugh Forte <hugh.forte@gmail.com>
2021-10-21 19:36:52 -0700

    feat(App): local storage- add user, retrieve users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { css } from '@emotion/react'
 import { Dropdown, Flex, Grid } from './components'
 
+const makeDate = () => {
+  const now = new Date()
+  const m = now.getMonth()
+  const d = now.getDate()
+  const y = now.getFullYear()
+  return `${m}/${d}/${y}`
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState(null)
+  const [userName, setuserName] = useState('')
+
+  useEffect(() => {
+    const existingUsers = localStorage.getItem('otd-users')
+    if (existingUsers) {
+      console.log('existingUsers', existingUsers)
+      setUsers(JSON.parse(existingUsers))
+    }
+  }, [])
+
+  const createNewUser = () => {
+    if (users) {
+      if (users.filter(userName) > -1) {
+        alert(`Oops, a user name ${userName} already exists.`)
+      } else {
+        const usersUpdate = users.concat([userName])
+        localStorage.setItem('otd-users', JSON.stringify(usersUpdate))
+        setUsers(usersUpdate)
+      }
+    } else {
+      localStorage.setItem('otd-users', JSON.stringify([userName]))
+      setUsers([userName])
+    }
+  }
+
   return (
     <Flex width="100%" height="100vh" position="relative">
       <Header>
         <h1 css={css({ fontSize: '1rem' })}>Out the Door</h1>
-        <Dropdown title="New user" placement="bottom-end">
+        <Dropdown title="+" placement="bottom-end">
           <Flex padding="8px">
-            <button type="button" onClick={() => setCount((c) => c + 1)}>
-              +
-            </button>
+            <form onSubmit={createNewUser}>
+              <Flex axis="vertical" gap="4px" alignment="end">
+                <label htmlFor="new-user-name" />
+                <input
+                  onChange={(e) => setuserName(e.target.value)}
+                  placeholder="Name"
+                  type="text"
+                  value={userName}
+                  autoFocus
+                />
+                <button type="submit">Create user</button>
+              </Flex>
+            </form>
           </Flex>
         </Dropdown>
       </Header>
       <BodyGrid>
-        {Array.from(new Array(count)).map((c, i) => (
-          <UserContainer key={i} />
-        ))}
+        {users &&
+          users.map((user, i) => <UserContainer key={i}>{user}</UserContainer>)}
       </BodyGrid>
     </Flex>
   )
@@ -56,7 +98,9 @@ const BodyGrid = ({ children }) => (
 )
 
 const UserContainer = ({ children }) => (
-  <Flex background="tomato" borderRadius="3px" height="15rem" />
+  <Flex background="tomato" borderRadius="3px" height="15rem">
+    {children}
+  </Flex>
 )
 
 export default App
