e1760fc85fa0b558b664e30ed565da4d2ef49b2a
Hugh Forte <hugh.forte@gmail.com>
2021-10-23 09:57:11 -0700

    refactor(Button): add basic variant control

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,8 +8,32 @@ export const Button = ({
   role = 'button',
   type,
   innerRef,
+  variant,
 }) => {
+  const getVariant = (variant) => {
+    switch (variant) {
+      case 'naked':
+        return {
+          background: 'transparent',
+          color: 'initial',
+          '&:hover': { background: '#E9E9E9' },
+        }
+      case 'outline':
+        return {
+          background: 'transparent',
+          color: 'initial',
+          border: '1px solid #3D3D3D',
+          '&:hover': { background: '#E9E9E9' },
+        }
+      default:
+        return {}
+    }
+  }
+
   const Button = css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     background: '#3D3D3D',
     appearance: 'none',
     border: '1px solid transparent',
@@ -18,11 +42,13 @@ export const Button = ({
     fontWeight: '600',
     letterSpacing: '.2px',
     padding: '4px 8px',
+    cursor: 'pointer',
     transition: 'opacity ease-in-out 150ms',
     opacity: 0.9,
     '&:hover': {
       opacity: 1,
     },
+    ...getVariant(variant),
   })
 
   return (
