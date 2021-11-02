82d820e6d7500512fb68cbad579bbac22fd836c2
Hugh Forte <hugh.forte@gmail.com>
2021-11-02 09:19:31 -0700

    refactor(Stack): convert Stack to styled system

--- src/components/layout/Stack.js
+++ src/components/layout/Stack.js
@@ -1,125 +1,36 @@
-import React from 'react'
-import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import { flex, system } from 'styled-system'
+import { Box } from '..'
 
-export const Stack = ({
-  alignment = 'start',
-  as: As = 'div',
-  axis,
-  background,
-  border = 'none',
-  borderBottomWidth = 0,
-  borderColor = 'transparent',
-  borderLeftWidth = 0,
-  borderRadius,
-  borderRightWidth = 0,
-  borderStyle = 'solid',
-  borderTopWidth = 0,
-  borderWidth = 0,
-  bottom,
-  boxShadow,
-  children,
-  className,
-  cursor,
-  distribution = 'start',
-  flex,
-  flexShrink,
-  gap,
-  grow,
-  height,
-  innerRef,
-  left,
-  margin,
-  marginBottom,
-  marginLeft,
-  marginRight,
-  marginTop,
-  maxHeight,
-  maxWidth,
-  minHeight,
-  minWidth,
-  onMouseEnter,
-  onMouseLeave,
-  padding,
-  paddingBottom,
-  paddingLeft,
-  paddingRight,
-  paddingTop,
-  position,
-  right,
-  top,
-  width,
-  ...restProps
-}) => {
-  let flexDirection
-
-  switch (axis) {
-    case 'horizontal':
-      flexDirection = 'row'
-      break
-    case 'vertical':
-      flexDirection = 'column'
-      break
-    default:
-      flexDirection = 'column'
-      break
-  }
-
-  const flexGrow = grow ? (typeof grow === 'boolean' ? 1 : grow) : 0
-
-  const Stack = {
-    alignItems: alignment,
-    background,
-    borderWidth,
-    border,
-    borderBottomWidth,
-    borderColor,
-    borderLeftWidth,
-    borderRadius,
-    borderRightWidth,
-    borderStyle,
-    borderTopWidth,
-    bottom,
-    boxShadow,
-    cursor,
-    display: 'flex',
-    flex,
-    flexDirection,
-    flexGrow,
-    flexShrink,
-    gap,
-    height,
-    justifyContent: distribution,
-    left,
-    margin,
-    marginBottom,
-    marginLeft,
-    marginRight,
-    marginTop,
-    maxHeight,
-    maxWidth,
-    minHeight,
-    minWidth,
-    padding,
-    paddingBottom,
-    paddingLeft,
-    paddingRight,
-    paddingTop,
-    position,
-    right,
-    top,
-    width,
-  }
-
-  return (
-    <As
-      className={className}
-      css={Stack}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      ref={innerRef}
-      {...restProps}
-    >
-      {children}
-    </As>
-  )
+const AXIS = {
+  horizontal: 'row',
+  vertical: 'column',
 }
+
+export const Stack = styled(Box)(
+  {
+    display: 'flex',
+    minWidth: 0,
+  },
+  system({
+    alignment: {
+      property: 'alignItems',
+      scale: 'alignment',
+    },
+    axis: {
+      property: 'flexDirection',
+      transform: (axis) => AXIS[axis],
+    },
+    distribution: {
+      property: 'justifyContent',
+      scale: 'distribution',
+    },
+    gap: {
+      properties: ['gap', 'gridGap'],
+      scale: 'gap',
+    },
+  }),
+  flex
+)
+
+Stack.defaultProps = { axis: 'vertical' }
