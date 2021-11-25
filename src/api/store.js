c6c3b2c67dc8f9d283e763ed231918dd1c2ce9ab
Hugh Forte <hugh.forte@gmail.com>
2021-11-25 12:52:47 -0800

    refactor(store): exports

--- src/api/store.js
+++ src/api/store.js
@@ -1,12 +1,10 @@
-export const STORE_KEYS = {
+const STORE_KEYS = {
   users: 'otd-users',
   date: 'otd-date',
 }
 
-export const set = (key, value) =>
-  localStorage.setItem(key, JSON.stringify(value))
+const set = (key, value) => localStorage.setItem(key, JSON.stringify(value))
 
-export const get = (key) => JSON.parse(localStorage.getItem(key))
+const get = (key) => JSON.parse(localStorage.getItem(key))
 
-const store = { get, set, STORE_KEYS }
-export default store
+export { get, set, STORE_KEYS }
