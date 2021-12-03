9577816e4d5f298c9c5771f07a2a98dd76f25b9e
Hugh Forte <hugh.forte@gmail.com>
2021-12-02 20:47:48 -0800

    chore(api): add supabaseClient

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
 export { set, get } from './store'
 export { getToday, makeToday, setToday } from './today'
 export { getUsers, setUsers } from './users'
+export { supabase } from './supabaseClient'
